3e5a86bc1ef492a35dcd025ce00a373f47f6c15f
Mike <wl02599509@gmail.com>
2022-06-24 16:49:41 +0800

    practice in class

---
+++ 01.js
@@ -0,0 +1,20 @@
+// console.log("hi") ;
+
+// alert("WARNING!")
+
+// var cat = "kitty"
+// var dog = "lai fu"
+// console.log(cat);
+// console.log(dog)
+
+
+function hi() {
+    console.log("hi")
+}
+
+hi()
+
+
+var age = 18
+console.log("I am " + age + " years old.")
+console.log(`I am ${age} years old.`)
