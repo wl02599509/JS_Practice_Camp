f5d837e023a0b315ddb4adb47a7f6bb51b2eea3b
Mike <wl02599509@gmail.com>
2022-07-09 15:08:50 +0800

    test_03_done

---
+++ candy_test/03_2.js
@@ -0,0 +1,27 @@
+// 編號：003
+// 程式語言：JavaScript
+// 題目：完成函數的內容，把陣列裡的 0 都移到最後面
+// 範例：
+
+let list = [false, 1, 0, -1, 2, 0, 1, 3, "a"]
+
+function moveZerosToEnd(arr) {
+    const zeroArr = arr.filter(num => num === 0)
+    //先把0從arr陣列挑出來成新陣列zeroArr
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            arr.splice(arr.indexOf(arr[i]), 1)
+        }
+    }
+    //再把arr陣列的0刪除
+    const mix = [...arr, ...zeroArr]
+    // const mix = arr.concat(zeroArr)
+    //兩組陣列展開並結合
+    return mix;
+}
+
+let result = moveZerosToEnd(list)
+console.log(result)  // 印出 [false, 1, -1, 2, 1, 3, "a", 0, 0]
+
+
+
