62133840d6ee59d4361758d563fb2e513abd2c61
Mike <wl02599509@gmail.com>
2022-07-09 15:15:03 +0800

    優化code

--- candy_test/03_2.js
+++ candy_test/03_2.js
@@ -8,13 +8,9 @@ let list = [false, 1, 0, -1, 2, 0, 1, 3, "a"]
 function moveZerosToEnd(arr) {
     const zeroArr = arr.filter(num => num === 0)
     //先把0從arr陣列挑出來成新陣列zeroArr
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === 0){
-            arr.splice(arr.indexOf(arr[i]), 1)
-        }
-    }
-    //再把arr陣列的0刪除
-    const mix = [...arr, ...zeroArr]
+    const noZero = arr.filter(num => num !== 0)
+    //再把arr裡非為0的元素挑出來成新陣列noZero
+    const mix = [...noZero, ...zeroArr]
     // const mix = arr.concat(zeroArr)
     //兩組陣列展開並結合
     return mix;
