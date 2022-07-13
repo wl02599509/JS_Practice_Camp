39188f9150ed0ddd6bd44c03b9ac40ad6b8b0d7b
Mike <wl02599509@gmail.com>
2022-07-13 10:11:37 +0800

    糖果題08完成

--- candy_test/08.js
+++ candy_test/08.js
@@ -6,10 +6,12 @@
 
 function highestScoreWord(input) {
     const sentenceArr = input.split('')
+    //將句子字串切割成一組字元字串，且包含空白字串的陣列
     const vacabularyArr = input.split(' ')
+    //將句子字串切割成一組單字字串的陣列
     let scoreArr = []
     let sum = 0
-
+    //將 sentenceArr 用 forEach，把一個個字元字串抓來轉換成 unicode 後減去96換算成題目分數，並進行相加;如果遇到空白字串元素或元素索引等於 sentenceArr 長度 -1 時（因為最後一個元素後面不會有空白），就把相加總和塞到新陣列 scoreArr 裡，並且重新計算新的總和。
     sentenceArr.forEach(elm => {
         if(elm == ' '| elm.indexOf() == sentenceArr.length -1){
             scoreArr.push(sum)
@@ -18,6 +20,7 @@ function highestScoreWord(input) {
             sum += (elm.charCodeAt() - 96)
         }
     });
+    //取得分數的陣列後，再將最大值取出指定給 biggest
     const biggest = scoreArr.reduce(function(acc, cv){
         if (acc < cv){
             return cv;
@@ -25,6 +28,7 @@ function highestScoreWord(input) {
             return acc;
         }
     })
+    //最後用indexOF將最大值於 scoreArr 的索引值套到 vacabularyArr，即可找到是哪個單字。
     return vacabularyArr[scoreArr.indexOf(biggest)];
 }
 
