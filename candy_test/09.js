fe9a98c2586d235baa546eb67897c013f32d6610
Mike <wl02599509@gmail.com>
2022-07-13 10:25:26 +0800

    糖果題09完成

--- candy_test/09.js
+++ candy_test/09.js
@@ -3,9 +3,10 @@
 // 題目：移除網址中的錨點（Anchor）
 
 function removeAnchor(url) {
-    // 實作寫在這裡
-  }
-  
-  console.log(removeAnchor("5xruby.tw")) // 印出 5xruby.tw
-  console.log(removeAnchor("5xruby.tw/#about")) // 印出 5xruby.tw/
-  console.log(removeAnchor("5xruby.tw/courses/?page=1#about")) // 印出 5xruby.tw/courses/?page=1
+    const websiteArr = url.split('#')
+    return websiteArr[0]
+}
+
+console.log(removeAnchor("5xruby.tw")) // 印出 5xruby.tw
+console.log(removeAnchor("5xruby.tw/#about")) // 印出 5xruby.tw/
+console.log(removeAnchor("5xruby.tw/courses/?page=1#about")) // 印出 5xruby.tw/courses/?page=1
