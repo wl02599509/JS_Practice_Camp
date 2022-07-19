eb7ed023b632af1ea3d9c56fdf7c2788e825959a
Mike <wl02599509@gmail.com>
2022-07-19 18:10:51 +0800

    note

--- shopping-cat-v1-main/src/scripts/main.js
+++ shopping-cat-v1-main/src/scripts/main.js
@@ -1,3 +1,60 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "@fortawesome/fontawesome-free/css/all.css"
-// 實作寫在這裡！
+
+// 貓咪按鈕
+
+const btn = document.querySelectorAll('.btn');
+const quantity = document.querySelector('.quantity');
+let cats = {}
+
+btn.forEach((elm) => elm.addEventListener('click', function(e){
+    const catName = e.target.closest('div').firstElementChild.textContent;
+
+    const catPrice = e.target.closest('div').firstChild.nextSibling.nextSibling.nextSibling.textContent
+
+    const tr = `<tr>
+                <td>${catName}</td>
+                <td><input type="number" class="quantity" value="1" /></td>
+                <td>${catPrice}</td>
+                <td id="total">$20</td>
+                <td>
+                    <button class="remove-item-btn btn btn-danger btn-sm">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </td>
+            </tr>`
+    
+    // if (abc) {
+    //     quantity.value = Number(quantity.value) + 1
+    // } 
+    // else  {
+        document.querySelector('tbody').insertAdjacentHTML('afterbegin', tr)
+    // }
+}))
+
+         
+
+
+
+
+
+
+// 刪除欄位 DONE
+
+const removeItem = document.querySelectorAll('.remove-item-btn');
+const removeItemIcon = document.querySelectorAll('.fa-trash-alt')
+const removeBtn = (e) => { 
+    e.target.parentNode.parentNode.remove() 
+}
+const removeBtnIcon = (e) => { e
+    .target.parentNode.parentNode.parentNode.remove() 
+}
+
+removeItem.forEach(function(btn) { btn.addEventListener('click', removeBtn) })
+removeItemIcon.forEach(function(btn) { btn.addEventListener('click', removeBtnIcon) })
+
+//總價顯示
+const total = document.querySelector(".total-price")
+const sum = document.querySelectorAll(".sum")
+
+
