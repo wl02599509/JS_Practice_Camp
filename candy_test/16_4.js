f78f307001fc9917a504a2b837742ed90b9824a2
Mike <wl02599509@gmail.com>
2022-07-20 00:14:07 +0800

    龍哥寫法

---
+++ candy_test/16_4.js
@@ -0,0 +1,21 @@
+// 編號：CANDY-016
+// 程式語言：JavaScript
+// 題目：把原本 snake_case 的字轉換成 camelCase 格式
+// 範例："hello_world" -> "helloWorld"
+
+function toCamelCase(str) {
+    return str.split('_') 
+              .map( (words, index) => {
+                    if ( index !== 0 ) {
+                        return words[0].toUpperCase() + words.slice(1)
+                    } else {
+                        return words;
+                    }
+                  })
+              .join('');
+              //合併
+}
+
+console.log(toCamelCase("book")) // book
+console.log(toCamelCase("book_store")) // bookStore
+console.log(toCamelCase("get_good_score")) // getGoodScore
