bff5ec58d6e12917308e0307f36b24bbb7b5157f
Mike <wl02599509@gmail.com>
2022-07-20 14:13:36 +0800

    貓咪購物按鈕完成，數量價格會更新，而欄位不會一直跑出新的。

--- shopping-cat-v1-main/src/scripts/main.js
+++ shopping-cat-v1-main/src/scripts/main.js
@@ -1,35 +1,48 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 
-// 貓咪按鈕
+// 貓咪加購按鈕
 
 const btn = document.querySelectorAll('.btn');
-const quantity = document.querySelector('.quantity');
+
 let cats = {}
 
 btn.forEach((elm) => elm.addEventListener('click', function(e){
     const catName = e.target.closest('div').firstElementChild.textContent;
-
     const catPrice = e.target.closest('div').firstChild.nextSibling.nextSibling.nextSibling.textContent
 
-    const tr = `<tr>
-                <td>${catName}</td>
-                <td><input type="number" class="quantity" value="1" /></td>
-                <td>${catPrice}</td>
-                <td id="total">$20</td>
-                <td>
-                    <button class="remove-item-btn btn btn-danger btn-sm">
-                        <i class="fas fa-trash-alt"></i>
-                    </button>
-                </td>
-            </tr>`
-    
-    // if (abc) {
-    //     quantity.value = Number(quantity.value) + 1
-    // } 
-    // else  {
-        document.querySelector('tbody').insertAdjacentHTML('afterbegin', tr)
-    // }
+    //去找 被創建出來的 tr 標籤 有沒有 id 為貓咪名字，有的話，代表在 td 裡已經有該貓咪的 tr 了，所以刪除掉，下面再創一個有新資訊的新欄位。
+    if (document.querySelector(`#${catName}`)) {
+        document.querySelector(`#${catName}`).remove()
+    }
+
+    //如果 cats物件裡沒有東西，那就在 cats物件裡做出，一個新的貓咪名字物件，內含名字價格等資訊。
+    if ((cats[`${catName}`] === undefined)) {
+        cats[`${catName}`] = {}
+        cats[`${catName}`].name = `${catName}`
+        cats[`${catName}`].price = `${catPrice}`
+        cats[`${catName}`].count = 1
+        cats[`${catName}`].total = Number((cats[`${catName}`].price).split('$').join(''))
+    //如果 cats物件裡同名的物件，就改變 count 跟 total 的值。
+    } else {
+        cats[`${catName}`].count = Number(cats[`${catName}`].count) + 1;
+        cats[`${catName}`].total = Number((cats[`${catName}`].price).split('$').join('')) * cats[`${catName}`].count;       
+    }
+
+    // 貓咪購物欄位
+    const tr = `<tr id="${catName}">
+                    <td>${cats[`${catName}`].name}</td>
+                    <td><input type="number" class="quantity" value="${cats[`${catName}`].count}" /></td>
+                    <td>${cats[`${catName}`].price}</td>
+                    <td id="total">${cats[`${catName}`].total}</td>
+                    <td>
+                        <button class="remove-item-btn btn btn-danger btn-sm">
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </td>
+                </tr>`
+    //在 tbody 裡插入 tr
+    document.querySelector('tbody').insertAdjacentHTML('afterbegin', tr);
 }))
 
          
---
+++ shopping-cat-v1-main/test.js
@@ -0,0 +1,16 @@
+// const cats = {
+//     boss: {
+//         name: 'boss',
+//         price: 20,
+//         count: 1
+//     }
+// }
+
+// cats.fat = {}
+// cats.fat.name = 'fat'
+// cats.fat.price = 20
+// cats.fat.count = 1
+// console.log(cats)
+
+const a = '$10'
+console.log(a.split('$').join(''))
